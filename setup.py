a57c2f0293bc80f4a89e79bd0bcc7398cd52d89c
roger <roger@elvex.org.ar>
2014-02-16 19:51:18 -0300

    remove ijson from setup.py

--- setup.py
+++ setup.py
@@ -1,9 +1,9 @@
 from setuptools import setup, find_packages
 
 setup(
-    name = "coucher",
-    version = "0.1",
-    install_requires=["requests", "ijson", "six"],
+    name="coucher",
+    version="0.1",
+    install_requires=["requests", "six"],
     packages=find_packages(),
     include_package_data=True,
     zip_safe=False,
